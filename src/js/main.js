e2c3bbe34d4cf44a9e317710e261117ae48c4a7d
apple <apple@192.168.5.5>
2018-01-13 17:10:26 +0800

    path

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2017/6/30.
+ */
+$('.go-top').click(function () {
+    console.log(1)
+    $(window).scrollTop(0)
+})
+$('.contact-store').click(function () {
+    location.href = '帮助中心-在线客服.html'
+})
