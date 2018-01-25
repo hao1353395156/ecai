519a9d1574bf09082ef9e213e47042bcbabb7e09
vic.tang <325381@qq.com>
2018-01-25 18:49:22 +0800

    order count

--- js/personal/my_order.js
+++ js/personal/my_order.js
@@ -3,7 +3,7 @@
 	var order = new Vue({
 	      el:"#my_order",
 	      data:{
-	            order_list:'',//消息列表
+	            order_list:[],//消息列表
 	            order_staus:0,//订单状态
 	            order_status_list:
 	            {
@@ -22,25 +22,26 @@
 	var url=config.API_GATEWAY + "/td/orders?pn=1&ps=20";
 	             Api.get(url,function(e){
 		            	if(e.code==0){
-		            	    order.order_list = e.data.items;
+		            	    //order.order_list = e.data.items;
 		            	    for (var i=0;i<e.data.items.length;i++){
-			            	   	if(e.data.items.status==0){
+			            	   	if(e.data.items[i].status==0){
 			            	   		order.order_status_list.dai++;
-			            	   	}
-			            	   	if(e.data.items.status==200){
+			            	   		order.order_list.push(e.data.items[i]);
+			            	   	}else if(e.data.items[i].status==200){
 			            	   		order.order_status_list.fa++;
-
-			            	   	}
-			            	   	if(e.data.items.status==300){
+			            	   		order.order_list.push(e.data.items[i]);
+			            	   	}else if(e.data.items[i].status==300){
 			            	   		order.order_status_list.shou++;
-
-			            	   	}
-			            	   	if(e.data.items.status==400){
+			            	   		order.order_list.push(e.data.items[i]);
+			            	   	}else if(e.data.items[i].status==400){
 			            	   		order.order_status_list.ping++;
-
+			            	   		order.order_list.push(e.data.items[i]);
 			            	   	}
-
+			            	   	
 		            	   }
+		            	   console.log(order.order_status_list);
+			            	   	//alert(order_status_list);
+
 		            	}
 		            });
 	window.Api = Api;
